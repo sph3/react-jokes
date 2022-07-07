47ba47a9c1845d08689b9d884f83b83604e27679
Sophia <sophialealbarros@gmail.com>
2022-07-07 20:50:59 -0300

    feat: add Roboto fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: ['./src/**/*.{ts,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif',
+      },
       colors: {
         rosewater: '#f5e0dc',
         flamingo: '#f2cdcd',
