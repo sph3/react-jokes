5b7fa30384e9566743cd3c7ffdc0176434ad061a
Sophia <sophialealbarros@gmail.com>
2022-07-07 21:46:17 -0300

    feat: ✨ create about component

---
+++ src/components/About.tsx
@@ -0,0 +1,12 @@
+export const About = () => {
+  return (
+    <div className='m-auto mt-4 px-6'>
+      <p className='text-subtext0 text-center'>
+        Welcome to React Jokes! <br />
+        It is a simple app that shows a joke everytime you reload the page.{' '}
+        <br />
+        And yes, they are all Chuck Norris jokes (because of my API choice).
+      </p>
+    </div>
+  );
+};
