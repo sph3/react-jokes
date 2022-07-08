5fe60c3dd6afe837689ec0da6ce4caf32cbaed26
Sophia <sophialealbarros@gmail.com>
2022-07-07 22:27:46 -0300

    style(about): 💎 remove useless whitespace
    I have no idea where it came from.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ export const About = () => {
     <div className='m-auto mt-4 px-6'>
       <p className='text-subtext0 text-center'>
         Welcome to React Jokes! <br />
-        It is a simple app that shows a joke everytime you reload the page.{' '}
+        It is a simple app that shows a joke everytime you reload the page.
         <br />
         And yes, they are all Chuck Norris jokes (because of my API choice).
       </p>
