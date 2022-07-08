38aec6d54ebbfb6471e30a2ddeea4c5f90d569a7
Sophia <sophialealbarros@gmail.com>
2022-07-07 22:12:28 -0300

    fix(container): 🐛 make text darker

--- src/components/JokeContainer.tsx
+++ src/components/JokeContainer.tsx
@@ -13,7 +13,9 @@ export const JokeContainer = () => {
 
   return (
     <div className='flex flex-col justify-center items-center mt-6 px-4'>
-      <span className='font-bold text-2xl'>So anyway, here's a joke:</span>
+      <span className='font-bold text-2xl text-subtext1'>
+        So anyway, here's a joke:
+      </span>
       <div className='w-full h-auto max-h-32 p-4 bg-mantle mt-4 rounded-2xl border-2 border-crust overflow-y-scroll'>
         <span className='text-subtext0 text-sm'>
           You better watch out, you better not cry; you better not pout, I'm
@@ -21,10 +23,15 @@ export const JokeContainer = () => {
           with every female member of your family.
         </span>
       </div>
-      <span className='font-bold text-2xl mt-6'>Did you like it?</span>
-      <span className='font-bold text-xl text-center mt-2'>
+      <span className='font-bold text-subtext1 text-2xl mt-6'>
+        Did you like it?
+      </span>
+      <span className='font-bold text-subtext1 text-xl text-center mt-2'>
         Well, you can always{' '}
-        <button onClick={reloadPage} className='text-blue focus:outline-none focus:underline hover:underline'>
+        <button
+          onClick={reloadPage}
+          className='text-blue focus:outline-none focus:underline hover:underline'
+        >
           reload the page
         </button>{' '}
         or click{' '}
