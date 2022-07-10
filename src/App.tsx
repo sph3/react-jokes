810e3e6dc60e9f9bcb235de10d2c9c4f41ae3ceb
Sophia <sophialealbarros@gmail.com>
2022-07-10 19:34:20 -0300

    fix(core): revert all theme changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import { useState } from 'react';
 import { About } from './components/About';
 import { Header } from './components/Header';
 import { JokeContainer } from './components/JokeContainer';
-import { ThemeChanger } from './components/ThemeChanger';
 
 export const App = () => {
-  let [themeChangerVisibility, setThemeChangerVisibility] = useState(false);
-  let [theme, setTheme] = useState('catppuccin-mocha');
-
-  const changeTheme = () => {};
-
   return (
-    <div>
-      <ThemeChanger changeThemeFunction={changeTheme} />
-      <Header
-        onClick={() => {
-          setThemeChangerVisibility(true);
-        }}
-      />
+    <>
+      <Header />
       <About />
       <JokeContainer />
-    </div>
+    </>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,13 @@
 import { SidebarSimple, Sun } from 'phosphor-react';
 
-interface IHeaderProps {
-  onClick: () => void;
-}
-
-export const Header = (props: IHeaderProps) => {
-  const handleClick = () => {
-    props.onClick();
-  };
-
+export const Header = () => {
   return (
     <header className='bg-mantle h-16 py-5 px-2 w-full flex items-center justify-between border-b border-crust'>
       <button className='bg-base h-10 w-10 rounded-xl flex justify-center items-center hover:brightness-[1.2]'>
         <SidebarSimple size={20} />
       </button>
       <span className='font-bold text-xl'>React Jokes 😀</span>
-      <button
-        onClick={handleClick}
-        className='bg-base h-10 w-10 rounded-xl flex justify-center items-center animate-pulse hover:animate-none hover:brightness-[1.2]'
-      >
+      <button className='bg-base h-10 w-10 rounded-xl flex justify-center items-center animate-pulse hover:animate-none hover:brightness-[1.2]'>
         <Sun size={20} />
       </button>
     </header>
